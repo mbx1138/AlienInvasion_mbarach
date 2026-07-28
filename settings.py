d15700475c3e23e3b6389633e7500d1f8b224240
mb <mbx11381@gmail.com>
2026-07-27 21:43:34 -0400

    removed background coloring

--- settings.py
+++ settings.py
@@ -6,7 +6,6 @@ class Settings:
         # Screen settings
         self.screen_width = 1200
         self.screen_height = 800
-        self.bg_color = (230, 230, 230)
 
         # Ship settings
         self.ship_limit = 3
