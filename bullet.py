1b365a230e6e39316998e27698761b64c631e8a5
mb <mbx11381@gmail.com>
2026-07-29 18:26:17 -0400

    added bullet image and Path in bullet class

--- bullet.py
+++ bullet.py
@@ -1,5 +1,8 @@
 import pygame
 from pygame.sprite import Sprite
+from pathlib import Path
+
+BULLET_PNG = Path("images/bullet.png")
 
 class Bullet(Sprite):
     """A class to manage bullets fired from the ship."""
@@ -9,11 +12,10 @@ class Bullet(Sprite):
         super().__init__()
         self.screen = ai_game.screen
         self.settings = ai_game.settings
-        self.color = self.settings.bullet_color
 
-        # Create a bullet rect at (0, 0) and then set correct position.
-        self.rect = pygame.Rect(0, 0, self.settings.bullet_width,
-            self.settings.bullet_height)
+        self.image = pygame.image.load(BULLET_PNG)
+        self.rect = self.image.get_rect()
+
         self.rect.midtop = ai_game.ship.rect.midtop
 
         # Store the bullet's position as a float.
@@ -27,5 +29,5 @@ class Bullet(Sprite):
         self.rect.y = self.y
 
     def draw_bullet(self):
-        """Draw the bullet to the screen."""
-        pygame.draw.rect(self.screen, self.color, self.rect)
+        """Add the bullet to the screen."""
+        self.screen.blit(self.image, self.rect)
