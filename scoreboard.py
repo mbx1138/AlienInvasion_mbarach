e36b59d1b2006049b111c4f348b03aaf4756e9fb
mb <mbx11381@gmail.com>
2026-08-12 14:42:12 -0400

    updated hud to top center of screen and color

--- scoreboard.py
+++ scoreboard.py
@@ -31,7 +31,7 @@ class Scoreboard:
         self.text_color = (30, 239, 230)
         #self.font = pygame.font.SysFont("ariel", 48)
         self.font = pygame.font.Font(
-            "Assets/Fonts/Silkscreen/Merriweather_24pt-ExtraBold.ttf", 58
+            "Assets/Fonts/Silkscreen/Merriweather_24pt-ExtraBold.ttf", 30
             )
 
         # Prepare the initial score images.
@@ -43,37 +43,37 @@ class Scoreboard:
     def prep_score(self):
         """Turn the score into a rendered image."""
         rounded_score = round(self.stats.score, -1)
-        score_str = f" SCORE:  {rounded_score:,}"
+        score_str = f"SCORE  {rounded_score:,}"
         self.score_image = self.font.render(score_str, True,
                 self.text_color)
 
         # Display the score at the top right of the screen.
         self.score_rect = self.score_image.get_rect()
-        self.score_rect.right = self.screen_rect.right - 20
-        self.score_rect.top = 20
+        self.score_rect.centerx = self.screen_rect.centerx + 100
+        self.score_rect.top = 15
 
     def prep_high_score(self):
         """Turn the high score into a rendered image."""
         high_score = round(self.stats.high_score, -1)
-        high_score_str = f" HIGH SCORE:  {high_score:,}"
+        high_score_str = f"HIGH SCORE  {high_score:,}"
         self.high_score_image = self.font.render(high_score_str, True,
                 self.text_color)
         
         # Center the high score at the top of the screen.
         self.high_score_rect = self.high_score_image.get_rect()
-        self.high_score_rect.centerx = self.screen_rect.centerx
-        self.high_score_rect.top = self.score_rect.top
+        self.high_score_rect.right = self.screen_rect.right - 20
+        self.high_score_rect.top = 15
 
     def prep_level(self):
         """Turn the level into a rendered image."""
-        level_str = str(self.stats.level)
+        level_str = f"LEVEL {str(self.stats.level)}"
         self.level_image = self.font.render(level_str, True,
                 self.text_color)
 
-        # Position the level below the score.
+        # Position the level to left of high score.
         self.level_rect = self.level_image.get_rect()
-        self.level_rect.right = self.score_rect.right
-        self.level_rect.top = self.score_rect.bottom + 10
+        self.level_rect.left = 285
+        self.level_rect.top = 15
 
     def prep_ships(self):
         """Show how many ships are left."""
